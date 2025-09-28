2ef062595d3d01bcdce23563fbfd70a9f959ffe8
AlexOFF <vladimiroff.all@yandex.ru>
2025-09-28 18:13:24 +0300

    feat: cors

---
+++ internal/middleware/cors.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+func CORSMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next(w, r)
+	}
+}
--- internal/router/router.go
+++ internal/router/router.go
@@ -4,24 +4,25 @@ import (
 	"net/http"
 
 	"github.com/go-park-mail-ru/2025_2_MindLeak/internal/handler"
+	"github.com/go-park-mail-ru/2025_2_MindLeak/internal/middleware"
 	"github.com/go-park-mail-ru/2025_2_MindLeak/internal/repository"
 )
 
 func NewRouter(sessions *repository.InMemorySession, users *repository.InMemoryUser, articles *repository.InMemoryArticle) *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/feed", middleware.CORSMiddleware(func(w http.ResponseWriter, r *http.Request) {
 		handler.FeedHandler(w, r, sessions, articles)
-	})
-	mux.HandleFunc("/registration", func(w http.ResponseWriter, r *http.Request) {
+	}))
+	mux.HandleFunc("/registration", middleware.CORSMiddleware(func(w http.ResponseWriter, r *http.Request) {
 		handler.RegistrationHandler(w, r, sessions, users)
-	})
-	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+	}))
+	mux.HandleFunc("/login", middleware.CORSMiddleware(func(w http.ResponseWriter, r *http.Request) {
 		handler.LoginHandler(w, r, sessions, users)
-	})
-	mux.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+	}))
+	mux.HandleFunc("/logout", middleware.CORSMiddleware(func(w http.ResponseWriter, r *http.Request) {
 		handler.LogoutHandler(w, r, sessions)
-	})
+	}))
 
 	return mux
 }
