a91cbe81aa58521fcdeb39c707b3537d000e03fb
Mikhail Tsapkov <matsapkov@gmail.com>
2025-09-29 04:43:43 +0300

    fix: cors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -5,7 +5,7 @@ import "net/http"
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
-		if origin == "http://localhost:3000" || origin == "http://localhost:5173" || origin == "http://127.0.0.1:3000" {
+		if origin == "http://localhost:3000" || origin == "http://localhost:5173" || origin == "http://127.0.0.1:3000" || origin == "http://62.109.19.84:8080" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
