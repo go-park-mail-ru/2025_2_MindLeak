418ec8efe052b6db0fcb25fd52d75a61a9501ea6
Mikhail Tsapkov <matsapkov@gmail.com>
2025-11-05 20:03:24 +0300

    fix: csrf

--- internal/middleware/csrf.go
+++ internal/middleware/csrf.go
@@ -31,9 +31,9 @@ func CSRFMiddleware(next http.Handler) http.Handler {
 					Name:     csrfCookieName,
 					Value:    token,
 					Path:     "/",
-					Secure:   true,
+					Secure:   false,
 					HttpOnly: false,
-					SameSite: http.SameSiteNoneMode,
+					SameSite: http.SameSiteLaxMode,
 					Expires:  time.Now().Add(1 * time.Hour),
 				})
 			}
