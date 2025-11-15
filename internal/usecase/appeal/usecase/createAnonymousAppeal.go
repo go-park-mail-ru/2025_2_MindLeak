7abebc474207c72de7f342cbff34a6373a7daf40
mikhailtsapkov <matsapkov@gmail.com>
2025-11-15 17:39:01 +0300

    fix: appeal update

--- internal/usecase/appeal/usecase/createAnonymousAppeal.go
+++ internal/usecase/appeal/usecase/createAnonymousAppeal.go
@@ -2,6 +2,8 @@ package usecase
 
 import (
 	"context"
+	"errors"
+	"strconv"
 
 	"github.com/go-park-mail-ru/2025_2_MindLeak/internal/handler/appeal/dto"
 	"github.com/go-park-mail-ru/2025_2_MindLeak/internal/models"
@@ -10,6 +12,11 @@ import (
 )
 
 func (u *Usecase) CreateAnonymousAppeal(ctx context.Context, appealDTO dto.AppealInputDto) (models.Appeal, error) {
+	catID, err := strconv.Atoi(appealDTO.CategoryID)
+	if err != nil {
+		return models.Appeal{}, errors.New("invalid category_id")
+	}
+
 	appeal := models.Appeal{
 		AppealID:           uuid.New(),
 		CreatorID:          uuid.New(),
@@ -17,7 +24,7 @@ func (u *Usecase) CreateAnonymousAppeal(ctx context.Context, appealDTO dto.Appea
 		Status:             models.Status(appealDTO.Status),
 		ProblemDescription: appealDTO.ProblemDescription,
 		Name:               appealDTO.Name,
-		CategoryID:         appealDTO.CategoryID,
+		CategoryID:         catID,
 		EmailForConnect:    appealDTO.EmailForConnect,
 		ScreenshotURL:      appealDTO.ScreenshotURL,
 	}
