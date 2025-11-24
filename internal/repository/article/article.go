4ae95490060bd0a8321c771814c236d78ce3c4cd
mikhailtsapkov <matsapkov@gmail.com>
2025-11-24 03:29:29 +0300

    fix: article repo

--- internal/repository/article/article.go
+++ internal/repository/article/article.go
@@ -345,36 +345,34 @@ func (r *ArticleRepo) loadTopic(ctx context.Context, a *models.Article) error {
 }
 
 func (r *ArticleRepo) SearchArticles(ctx context.Context, queryText string) ([]models.Article, error) {
-	query := `SELECT 
-			a.article_id, a.author_id, a.title, a.content, a.media_url,
-			a.topic_id, a.status, a.comments_count, a.reposts_count, a.views_count,
-			a.created_at, a.updated_at,
-			u.name AS author_name,
-			u.avatar AS author_avatar,
-			t.title AS topic_title
-		FROM article a
-		LEFT JOIN "user" u ON u.user_id = a.author_id
-		LEFT JOIN topic t ON t.topic_id = a.topic_id
-		WHERE (a.title ILIKE '%' || $1 || '%' OR a.content ILIKE '%' || $1 || '%')
-		LIMIT 20;
-		`
-	logger.Warn(ctx, "SearchArticles: queryText=%q", queryText)
+	query := `
+        SELECT 
+            article_id, author_id, title, content, media_url,
+            topic_id, status, comments_count, reposts_count, views_count,
+            created_at, updated_at
+        FROM article
+        WHERE 
+            (title ILIKE '%' || $1 || '%' OR content ILIKE '%' || $1 || '%')
+        LIMIT 20;
+    `
 
-	articles := make([]models.Article, 0)
 	rows, err := r.db.QueryContext(ctx, query, queryText)
 	if err != nil {
-		logger.Error(ctx, err.Error())
 		return nil, fmt.Errorf("search articles: %w", err)
 	}
 	defer rows.Close()
+
+	articles := make([]models.Article, 0)
 	for rows.Next() {
-		a := models.Article{}
+		var a models.Article
+		var mediaURL sql.NullString
+
 		err = rows.Scan(
 			&a.ID,
 			&a.AuthorID,
 			&a.Title,
 			&a.Content,
-			&a.MediaURL,
+			&mediaURL,
 			&a.TopicID,
 			&a.Status,
 			&a.CommentsCount,
@@ -382,13 +380,15 @@ func (r *ArticleRepo) SearchArticles(ctx context.Context, queryText string) ([]m
 			&a.ViewsCount,
 			&a.CreatedAt,
 			&a.UpdatedAt,
-			&a.AuthorName,
-			&a.AuthorAvatar,
-			&a.Topic.Title,
 		)
-
 		if err != nil {
 			return nil, fmt.Errorf("SearchArticles scan error: %w", err)
+		}
+
+		if mediaURL.Valid {
+			a.MediaURL = mediaURL.String
+		} else {
+			a.MediaURL = ""
 		}
 
 		articles = append(articles, a)
